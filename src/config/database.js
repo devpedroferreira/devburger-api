2a4261397cb7611809a50b7f3595b6b1ae54070d
devpedroferreira <pedroofan@gmail.com>
2025-02-03 10:36:07 -0300

    config /config/database.js for manage postgresDB

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 2345,
+    username: 'postgres',
+    password: '2310postgres',
+    database: 'devburger',
+    define: {
+        timestamps: true,
+    },
+};
