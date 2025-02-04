161df2efe7ab8931385277dcd430408ed34edcab
devpedroferreira <pedroofan@gmail.com>
2025-02-04 11:27:28 -0300

    Added validadores ao models User.js

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,2 +1,30 @@
-import Sequelize, {Model} from 'sequelize-cli'
+class User extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                name: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                    validate: {
+                        notEmpty: true,
+                    },
+                },
+                email: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                    unique: true,
+                    validate: {
+                        isEmail: true,
+                    },
+                },
+                password_hash: Sequelize.STRING,
+                admin: Sequelize.BOOLEAN,
+            },
+            {
+                sequelize,
+            }
+        );
+    }
+};
 
+export default User;
