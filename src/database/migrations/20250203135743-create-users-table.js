9a26ed66d58b4a9e9d5e9a122dc2b5b5555d8ab1
devpedroferreira <pedroofan@gmail.com>
2025-02-04 00:39:07 -0300

    config sequelice-cli for use ES module

--- src/database/migrations/20250203135743-create-users-table.js
+++ src/database/migrations/20250203135743-create-users-table.js
@@ -1,56 +1,52 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:*/
-     await queryInterface.createTable('users', {
-       id:{
-          primaryKey: true,
-          allowNull: false, // diz que o campo é obrigatorio
-          type: Sequelize.UUID,
-          defaultValue: Sequelize.UUIDV4,
-          },
+export async function up(queryInterface, Sequelize) {
+  /**
+   * Add altering commands here.
+   *
+   * Example:*/
+  await queryInterface.createTable('users', {
+    id: {
+      primaryKey: true,
+      allowNull: false, // diz que o campo é obrigatorio
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+    },
 
-          name:{
-            type: Sequelize.STRING,
-            allowNull: false,
-          },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
 
-          email:{
-            type: Sequelize.STRING,
-            allowNull: false,
-          },
-          
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
 
-          password_hash:{
-            type: Sequelize.STRING,
-            allowNull: false, 
-          },
 
-          admin:{
-            type: Sequelize.BOOLEAN,
-            defaultValue: false,
-          },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
 
-          created_at:{
-            type: Sequelize.DATE,
-            allowNull: false,
-          }, 
-          updated_at:{
-            type: Sequelize.DATE,
-            allowNull: false,
-          }
-       
-        });
-     
-         },
+    admin: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
 
-  async down (queryInterface, Sequelize) {
-     await queryInterface.dropTable('users');
-    
-  }
-};
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    }
+  });
+
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('users');
+
+}
