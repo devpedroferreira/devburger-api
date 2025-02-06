46a274c326bb5c16e102b25b464c419893385b2e
devpedroferreira <pedroofan@gmail.com>
2025-02-06 11:17:55 -0300

    config UserController validation with yup, senha min 6 charac

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,11 +10,23 @@ delete => delete
 
 import {v4} from 'uuid';
 import User from '../../app/models/User.js';
+import * as Yup from 'yup';
 
 class UserController {
     async store(req, res) {
         // try catach to handle error
         try {
+            // vilid data with yup lib            
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Digite o nome corretamente.'),
+                email: Yup.string().email('Digite um email valido exemplo@email.com').required('email obrigatório'),
+                password_hash: Yup.string().min(6, 'senha 6 caractere minimo').required('senha obrigatório'),
+                admin: Yup.boolean()
+            });
+
+            // validar todos os campos e retornar todos os erros encontrados.
+            await schema.validate(req.body, {abortEarly: false});
+
             const {name, email, password_hash, admin} = req.body;
             const user = await User.create({
                 id: v4(),
@@ -28,8 +40,13 @@ class UserController {
                 name,
                 email,
                 admin
-            })
+            });
         } catch (error) {
+            // retorna os erros : await schema.validate(res.body, {abortEarly: false});
+            if (error.name === 'ValidationError') {
+                return res.status(400).json({error: error.errors});
+            }
+            // erro email
             if (error.name === 'SequelizeUniqueConstraintError') {
                 return res.status(400).json({ error: 'Email already exists' });
             }
