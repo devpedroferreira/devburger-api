0a695785144256280f007b1490af106a3c0c89d1
devpedroferreira <pedroofan@gmail.com>
2025-02-08 00:56:51 -0300

    conf SessionController.js compare password, its OK, tested with postman

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,15 +1,49 @@
 // validando dados do usuario para login/sessao na aplicacao
 import * as Yup from "yup";
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 class SessionController {
     async store(req, res){
-        const schema = Yup.object({
+        const schema = Yup.object().shape({
             email: Yup.string().email('Digite um email valido, exemplo@email.com ').required('Email é obrigatório'),
-            password: Yup.string().min(6,"A senha deve conter no minímo 6 character.").required(),
+            password: Yup.string().min(6,"A senha deve conter no minímo 6 character.").required('Senha é obrigatório'),
         });
 
+        // validated if email and pass are in the correct format.
+        try {
+            await schema.validate(req.body, {abortEarly: false});
+        } catch (err) {
+            return res.status(401).send('Email or password incorrect!');
+        }
+        
+        // date user
         const { email, password } = req.body;
 
+        try{
+        // Busca usuario pelo email
+        const user = await User.findOne({where: {email} });
+
+        if (!user){
+            return res.status(401).json({error: "Email ou senha incorretos!"});
+        };
+
+        // verificando se a senha coincide com o hash
+        if(!(await bcrypt.compare(password, user.password_hash))){
+            return res.status(401).json({error: "Email ou senha incorretos!"});
+        };
+
+        //Se tudo estiver correto, retorna os dados do usuaro
+        return res.json({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            admin: user.admin
+        });
+
+        } catch (error) {
+            return res.status(500).json({ error: "Erro interno no servidor" });
+        };
 
     };
 };
