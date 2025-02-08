2c7bdb95c816db3ca394ae488c2a8f773fc9ec2a
devpedroferreira <pedroofan@gmail.com>
2025-02-07 23:34:06 -0300

    conf SessionController.js -> validated if the email and pass are in the correct format, if not, send the menssage: email or pass incorrect.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,17 @@
+// validando dados do usuario para login/sessao na aplicacao
+import * as Yup from "yup";
+
 class SessionController {
     async store(req, res){
-        return res.send('TEste')
-    }
-}
+        const schema = Yup.object({
+            email: Yup.string().email('Digite um email valido, exemplo@email.com ').required('Email é obrigatório'),
+            password: Yup.string().min(6,"A senha deve conter no minímo 6 character.").required(),
+        });
+
+        const { email, password } = req.body;
+
+
+    };
+};
 
 export default new SessionController();
