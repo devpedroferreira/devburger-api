ff973bd8cedf2b47c222188fafbfb6c4cdf62a9c
devpedroferreira <pedroofan@gmail.com>
2025-02-08 10:12:26 -0300

    conf ../migrations/create-products-table

--- src/database/migrations/20250208122127-create-products-table.js
+++ src/database/migrations/20250208122127-create-products-table.js
@@ -3,20 +3,47 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    
+      await queryInterface.createTable('products', { 
+        id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      category : {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      // path da img do produto
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+
+      });
+    
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface) {
+    
+      await queryInterface.dropTable('products');
+    
   }
 };
