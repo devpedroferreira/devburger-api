3bef05d751355cdacd7de96fcf2edb542f52caf6
devpedroferreira <pedroofan@gmail.com>
2025-02-07 22:17:35 -0300

    created /src/app/controllers/SessionController

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController.js";
+import SessionController from "./app/controllers/SessionController.js";
 
 const router = Router();
 // rota post
 router.post('/users', UserController.store);
+
+// session
+router.post('/session', SessionController)
 
 //exportando as rotas
 export default router;
