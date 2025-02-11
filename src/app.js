dfc8461c109391591bc0460fa7f8177782e4cc0a
devpedroferreira <pedroofan@gmail.com>
2025-02-11 09:51:24 -0300

    conf app.js in middlewares route get por all products

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import routes from './routes.js'; // Importe suas rotas aqui
 import './database/index.js';
+
+import {resolve} from 'node:path';
+
 class App {
     constructor() {
         this.app = express(); // Cria a instância do Express
@@ -10,6 +13,7 @@ class App {
 
     middlewares() {
         this.app.use(express.json()); // Middleware para parsear JSON
+        this.app.use('/product-file', express.static(resolve('..','uploads')));
     }
 
     routes() {
