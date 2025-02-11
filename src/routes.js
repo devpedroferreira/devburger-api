7d3013d4bd6c468f2acb6dd375870fed8c8d99d1
devpedroferreira <pedroofan@gmail.com>
2025-02-10 23:30:38 -0300

    update routes.js conf multer upload

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,11 @@ import { Router } from "express";
 import UserController from "./app/controllers/UserController.js";
 import SessionController from "./app/controllers/SessionController.js";
 import ProductController from "./app/controllers/ProductController.js";
+import multer from "multer";
+import multerConfig from './config/multer.js';
+
+// conf multer
+const upload = multer(multerConfig);
 
 const router = Router();
 // rota post
@@ -9,7 +14,7 @@ router.post('/users', UserController.store);
 // session
 router.post('/session', SessionController.store);
 // Product
-router.post('products', ProductController.store);
+router.post('products', upload.single('file'), ProductController.store);
 
 //exportando as rotas
 export default router;
