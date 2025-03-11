10ca7c93594b6bb99c98452befedba75d15500f7
devpedroferreira <pedroofan@gmail.com>
2025-03-11 00:26:23 -0300

    add path img in model Category

---
+++ migrations/20250311031400-add-img-category-table.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.addColumn('category', 'path', {
+    type: Sequelize.STRING,
+    allowNull: true,
+  });
+}
+
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.removeColumn('category', 'path');
+};
--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -1,26 +1,43 @@
-import {Model, Sequelize} from 'sequelize';
+import { Model, Sequelize } from 'sequelize';
 
 class Category extends Model {
-    static init(sequelize){
+    static init(sequelize) {
         super.init(
-        {
-            name: {
-                type: Sequelize.STRING,
-                unique: true
+            {
+                name: {
+                    type: Sequelize.STRING,
+                    unique: true,
+                    allowNull: false,
+                },
+                description: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                path: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                url: {
+                    type: Sequelize.VIRTUAL,
+                    get() {
+                        return this.path
+                            ? `http://localhost:5433/category-file/${this.path}`
+                            : null;
+                    },
+                },
             },
-            description: Sequelize.STRING,   
-        },
             {
                 sequelize,
-                tableName:'category', // especifica o nome da tabela
-            },
+                tableName: 'category', // specifies the table name
+            }
         );
 
         return this;
-    };
-    static associations(models){
-        this.hasMany(models.Product, {foreignKey: 'category_id', as: 'products'});
-    };
-};
+    }
+
+    static associate(models) {
+        this.hasMany(models.Product, { foreignKey: 'category_id', as: 'products' });
+    }
+}
 
 export default Category;
